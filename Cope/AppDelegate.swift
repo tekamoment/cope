805eb5b7c52d2e25cd6084d44d69150b0bb7ab55
buddybuild <help@buddybuild.com>
2016-10-07 03:43:11 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Cope/AppDelegate.swift
+++ Cope/AppDelegate.swift
@@ -21,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         UINavigationBar.appearance().isOpaque = true
         UINavigationBar.appearance().barTintColor = UIColor(hexString: "F38A8A")
